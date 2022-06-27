d5deaa3f116759aafb7af1205a2b5ad452119f76
Marlee Garcia <marlee.garcia121@gmail.com>
2022-06-27 19:03:24 -0400

    Tested different greeting

--- tests/greeter.test.ts
+++ tests/greeter.test.ts
@@ -4,10 +4,21 @@ describe("Greeter class", () => {
     test("Hello, Marlee", () => {
         let greetingPhrase = "Hello";
         let name = "Marlee";
-        let greeting = new Greeter(greetingPhrase)
+        let greeting = new Greeter(greetingPhrase);
 
         let fullGreeting = greeting.greet(name);
 
         expect(fullGreeting).toBe("Hello, Marlee");
-    }) 
-})
+    });
+    
+    test("Howdy, partner", () => {
+        let greetingPhrase = "Howdy";
+        let name = "partner";
+        let greeting = new Greeter(greetingPhrase);
+
+        let fullGreeting = greeting.greet(name);
+
+        expect(fullGreeting).toBe("Howdy, partner");
+    });
+});
+
